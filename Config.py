ba5c36f9680378467cfb9a77db5f6cfbd76c0e6c
ShaoShaoqi <shaosq@whut.edu.cn>
2023-12-21 16:55:44 +0800

    EPOCH MODIFY

--- Config.py
+++ Config.py
@@ -11,7 +11,7 @@
 class hyperparameter():
     def __init__(self):
         self.Learning_rate = 1e-4
-        self.Epoch = 20
+        self.Epoch = 200
         self.Batch_size = 16
         self.Patience = 20
         self.decay_interval = 10
